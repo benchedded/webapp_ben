e1744b26a0c1e4858e6d38ab45ea3147f08f5491
Ho Pang Ming Benjamin <242276Z@mymail.nyp.edu.sg>
2025-08-03 00:34:31 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request, redirect, url_for # type: ignore
+import calendar
+from datetime import datetime
+import shelve, module
+from createModuleForm import module_form
+
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/resources')
+def resources():
+    return render_template('resources.html', )
+
+@app.route('/learningModules')
+def learningModules():
+    return render_template('learningModules.html')
+
+'''
+
+@app.route('/calendar')
+def calendar_view():
+    now = datetime.now()
+    year = now.year
+    month = now.month
+
+    # Get calendar matrix for the month
+    cal = calendar.Calendar(firstweekday=6)  # Sunday as first day
+    month_days = cal.monthdayscalendar(year, month)
+
+    month_name = calendar.month_name[month]
+
+    return render_template('calendar.html', month=month_name, year=year, month_days=month_days)
+
+'''
+
+@app.route('/assignModule', methods = ['GET','POST'])
+def assign_module():
+    assign_module = module_form(request.form)
+    if request.method == 'POST' and assign_module.validate():
+        module_dict = {}
+        db = shelve.open('storage_database/storage.db','c')
+
+        try:
+            module_dict = db['Modules']
+        except:
+            print('Error in retriving modules')
+        
+        new_module = module.Module(
+            assign_module.module_type.data,
+            assign_module.module_num.data
+        )
+        module_dict[(new_module.get_module_assign_id())] = new_module
+        db['Modules'] = module_dict
+
+        db.close()
+        return redirect(url_for('learningModules'))
+
+    return render_template('assignModule.html', form = assign_module)
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
