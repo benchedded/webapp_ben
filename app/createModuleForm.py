cfa702923e863539c9a2ee84c2190c2b98632b26
Ho Pang Ming Benjamin <242276Z@mymail.nyp.edu.sg>
2025-08-08 22:41:04 +0800

    Delete app directory

--- app/createModuleForm.py
+++
@@ -1,7 +0,0 @@
-from wtforms import Form, StringField, RadioField, SelectField,TextAreaField, validators #type: ignore
-
-class module_form(Form):
-    module_type = SelectField('Module Type', [validators.DataRequired()], choices=[('', 'Select'),('S', 'Seizure'),('MD', 'Mood Disorders'), ('N', 'Nutrition'),('SI','Sleep Issues')], default='')
-    module_num = SelectField('Module Type', [validators.DataRequired()], choices=[('','Select'),('1', '1'),('2', '2'), ('3', '3'),('4','4')], default='')
-
-
